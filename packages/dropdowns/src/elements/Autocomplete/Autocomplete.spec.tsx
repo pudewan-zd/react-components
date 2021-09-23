193cf981986a389fd5aa73c76bb605f267a152ba
Smriti Madan <smriti.madan@zendesk.com>
2021-09-23 12:49:48 -0400

    wip: improve tests for dropdowns

--- packages/dropdowns/src/elements/Autocomplete/Autocomplete.spec.tsx
+++ packages/dropdowns/src/elements/Autocomplete/Autocomplete.spec.tsx
@@ -5,7 +5,8 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import debounce from 'lodash.debounce';
 import userEvent from '@testing-library/user-event';
 import { render, fireEvent } from 'garden-test-utils';
 import { Dropdown, Autocomplete, Field, Menu, Item, Label } from '../..';
@@ -155,6 +156,72 @@ describe('Autocomplete', () => {
 
       userEvent.type(autocomplete.querySelector('input')!, '{escape}');
       expect(autocomplete).not.toHaveClass('is-open');
+    });
+
+    it('input value matches selection', () => {
+      const options = ['Asparagus', 'Brussel sprouts', 'Cauliflower', 'Garlic'];
+      const Example = () => {
+        const [selectedItem, setSelectedItem] = useState('');
+        const [inputValue, setInputValue] = useState('');
+        const [matchingOptions, setMatchingOptions] = useState(options);
+
+        /**
+         * Debounce filtering
+         */
+        const filterMatchingOptionsRef = useRef(
+          debounce((value: string) => {
+            const matchedOptions = options.filter(
+              option => option.trim().toLowerCase().indexOf(value.trim().toLowerCase()) !== -1
+            );
+
+            setMatchingOptions(matchedOptions);
+          }, 0)
+        );
+
+        useEffect(() => {
+          filterMatchingOptionsRef.current(inputValue);
+        }, [inputValue]);
+
+        return (
+          <Dropdown
+            inputValue={inputValue}
+            selectedItem={selectedItem}
+            onSelect={item => {
+              setSelectedItem(item);
+              setInputValue(item);
+            }}
+            onInputValueChange={value => setInputValue(value)}
+            downshiftProps={{ defaultHighlightedIndex: 0 }}
+          >
+            <Field>
+              <Label>Choose a vegetable</Label>
+              <Autocomplete data-test-id="autocomplete">{selectedItem}</Autocomplete>
+            </Field>
+            <Menu data-test-id="menu">
+              {matchingOptions.length ? (
+                matchingOptions.map(option => (
+                  <Item key={option} value={option} data-test-id="item">
+                    <span>{option}</span>
+                  </Item>
+                ))
+              ) : (
+                <Item disabled>No matches found</Item>
+              )}
+            </Menu>
+          </Dropdown>
+        );
+      };
+      const { getByTestId, debug } = render(<Example />);
+      const autocomplete = getByTestId('autocomplete');
+
+      userEvent.click(autocomplete);
+
+      userEvent.tab();
+
+      fireEvent.keyDown(autocomplete, { key: 'ArrowUp', keyCode: 38 });
+      debug(document.activeElement as any);
+
+      expect(true).toBe(true);
     });
   });
 });
